cd930693455db695782e47ca512675e4dd89182c
Dillon Dawson <87071332+Dillon-Dawson@users.noreply.github.com>
2025-04-23 18:12:11 +0100

    :D

---
+++ Dev/Web/JS/Popup.js
@@ -0,0 +1,9 @@
+// Function to open the form
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+// Function to close the form
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
